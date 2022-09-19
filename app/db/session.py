04b8d27ff56c6554948c62f58ffe0c41fb4c8698
ArnoldoRicardo <anyback@gmail.com>
2022-09-19 16:47:01 -0500

    add db

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,11 @@
 import os
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL'].replace(
-#     'postgres://', 'postgresql://')
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL'].replace(
+    'postgres://', 'postgresql://')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
